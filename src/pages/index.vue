85488e6046864e4315f2ca16049a9eae208cfc75
Andrew Hsieh <hrh47.83@gmail.com>
2022-12-04 19:34:31 +0800

    Finish exercise1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,13 +2,11 @@
 <template>
   <div class="flex justify-center items-center h-[80vh]">
     <div class="text-center">
-      <img
-        class="block"
-        src="https://vuejsforge.com/images/logo.svg"
-        alt="Vue.js Forge"
-      />
+      <img class="block" src="../../public/logo.svg" alt="Vue.js Forge" />
       <AppButton class="block">
-        <a href="https://vi.to/hubs/vuejs-forge/" target="_blank">Get Started</a>
+        <a href="https://vi.to/hubs/vuejs-forge/" target="_blank"
+          >Get Started</a
+        >
       </AppButton>
     </div>
   </div>
