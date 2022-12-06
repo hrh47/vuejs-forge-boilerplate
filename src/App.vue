48be6e0b9dcbd956c8477db242e35634be204650
Andrew Hsieh <andrew@lawsnote.com>
2022-12-06 18:09:04 +0800

    Finish exercise2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheDrawer from '@/components/TheDrawer.vue';
+import TheNavbar from '@/components/TheNavbar.vue';
+</script>
 
 <template>
-  <RouterView />
+  <TheNavbar />
+  <TheDrawer />
 </template>
---
+++ src/components/TheDrawer.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <Drawer
+      class="h-[90vh]"
+      :expanded="expanded"
+      position="start"
+      mode="push"
+      :mini="true"
+      :items="items"
+      @select="onSelect"
+    >
+      <DrawerContent>
+        <div class="px-5">
+          <router-view />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Drawer, DrawerContent } from '@progress/kendo-vue-layout';
+import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+// const selectedId = ref(0);
+const expanded = ref(false);
+const expandedIcon = computed(() => {
+  return expanded.value ? 'k-i-arrow-chevron-left' : 'k-i-arrow-chevron-right';
+});
+const items = computed(() => [
+  {
+    text: 'Boards',
+    icon: 'k-i-set-column-position',
+    selected: true,
+    data: {
+      path: '/'
+    }
+  },
+  {
+    text: 'Templates',
+    icon: 'k-i-border-left',
+    data: {
+      path: '/templates'
+    }
+  },
+  {
+    text: 'Settings',
+    icon: 'k-i-gear',
+    data: {
+      path: '/settings'
+    }
+  },
+  {
+    text: 'Collapse',
+    icon: expandedIcon.value,
+    data: {
+      action: () => (expanded.value = !expanded.value)
+    }
+  }
+]);
+const router = useRouter();
+const onSelect = ({ itemIndex }: { itemIndex: number }) => {
+  const item = items.value[itemIndex];
+  if (item.data.path) {
+    router.push(item.data.path);
+  }
+  if (typeof item.data.action === 'function') {
+    item.data.action();
+  }
+};
+</script>
+
+<style scoped></style>
