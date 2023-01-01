686335541945095b9e95611847eb4c6b5dd5cf37
Andrew Hsieh <hrh47.83@gmail.com>
2023-01-01 19:46:57 +0800

    Fix error

--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -24,45 +24,51 @@ import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
 import { useLocalStorage } from "@vueuse/core";
 
+const selectedId = ref(0);
 const expanded = ref(useLocalStorage("vue-forge-drawer-expanded", true));
 const expandedIcon = computed(() => {
   return expanded.value ? "k-i-arrow-chevron-left" : "k-i-arrow-chevron-right";
 });
-const items = computed(() => [
-  {
-    text: "Boards",
-    icon: "k-i-set-column-position",
-    selected: true,
-    data: {
-      path: "/boards",
+const items = computed(() =>
+  [
+    {
+      text: "Boards",
+      icon: "k-i-set-column-position",
+      data: {
+        path: "/boards",
+      },
     },
-  },
-  {
-    text: "Templates",
-    icon: "k-i-border-left",
-    data: {
-      path: "/templates",
+    {
+      text: "Templates",
+      icon: "k-i-border-left",
+      data: {
+        path: "/templates",
+      },
     },
-  },
-  {
-    text: "Settings",
-    icon: "k-i-gear",
-    data: {
-      path: "/settings",
+    {
+      text: "Settings",
+      icon: "k-i-gear",
+      data: {
+        path: "/settings",
+      },
     },
-  },
-  {
-    text: "Collapse",
-    icon: expandedIcon.value,
-    data: {
-      action: () => (expanded.value = !expanded.value),
+    {
+      text: "Collapse",
+      icon: expandedIcon.value,
+      data: {
+        action: () => (expanded.value = !expanded.value),
+      },
     },
-  },
-]);
+  ].map((item, index) => ({
+    ...item,
+    selected: index === selectedId.value,
+  }))
+);
 const router = useRouter();
 const onSelect = ({ itemIndex }: { itemIndex: number }) => {
   const item = items.value[itemIndex];
   if (item.data.path) {
+    selectedId.value = itemIndex;
     router.push(item.data.path);
   }
   if (typeof item.data.action === "function") {
